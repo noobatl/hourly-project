a9e19119c732e55b69ccbb3cf2ad0b9476879216
Kseredick95 <kseredick95@gmail.com>
2020-06-04 21:08:36 -0400

    Created api-routes.js folder

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 
-var express = require("express");
+const express = require("express");
 
-var app = express();
-var PORT = process.env.PORT || 8080;
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 
 //Create files for each table in database -- UNCOMMENT AFTER DATABASE IS SET
-//var db = require("./app/models");
+//const db = require("./app/models");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,7 +16,7 @@ app.use(express.static("public"));
 
 
 //Routing for site
-//require("./routes/api-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
 //UNCOMMENT WHEN DATABASES IN MODELS ARE SET-UP
 // db.sequelize.sync().then(function() {
