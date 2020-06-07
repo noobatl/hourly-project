16b45e71108df38c9769bc3cec4a321196e0980e
Nicole OBomsawin <nicoleobomsawin@gmail.com>
2020-06-07 15:55:27 -0400

    adding model task

---
+++ app/models/task.js
@@ -0,0 +1,16 @@
+module.exports = function(sequelize, DataTypes) {
+    var Task = sequelize.define("Task", {
+        taskID: {
+            type: DataTypes.INT,
+            autoIncrement: true,
+            primaryKey: true,
+
+        },
+        taskName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+
+    })
+}
