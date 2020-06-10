12cb518303f3da422457944a5bd2607d19e41e00
noobatl <info@noobusa.com>
2020-06-09 20:04:48 -0400

    restored changes from master

--- app/models/projects.js
+++ app/models/projects.js
@@ -1,9 +1,9 @@
 module.exports = function (sequelize, DataTypes) {
     const Project = sequelize.define("Project", {
         name : DataTypes.STRING,
-        budget : DataTypes.INTEGRAL,
+        budget : DataTypes.INTEGER,
         description : DataTypes.STRING,
-        status : boolean
+        status : DataTypes.BOOLEAN
     })
 
     return Project;
--- server.js
+++ server.js
@@ -23,7 +23,7 @@ db.sequelize.sync().then(function() {
   });
 });
 
-app.listen(PORT, function () {
-  console.log("App listening on PORT " + PORT);
-});
+// app.listen(PORT, function () {
+//   console.log("App listening on PORT " + PORT);
+// });
 
