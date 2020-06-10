e3fd436a6924af0964a806022cba0f84b28344a7
Nicole OBomsawin <nicoleobomsawin@gmail.com>
2020-06-10 09:17:23 -0400

    editing task.js

--- app/models/task.js
+++ app/models/task.js
@@ -17,7 +17,7 @@ module.exports = function(sequelize, DataTypes) {
             type: DataTypes.INTEGER,
         },
         taskDescription: {
-            type: DataTypes.STRING,
+            type: DataTypes.TEXT,
         }
     });
 
