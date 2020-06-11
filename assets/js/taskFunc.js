281968bfa9711c086a4ff3f7fc370667effe73b4
Nicole OBomsawin <nicoleobomsawin@gmail.com>
2020-06-11 12:17:01 -0400

    removing old comments in doc

--- assets/js/taskFunc.js
+++ assets/js/taskFunc.js
@@ -48,7 +48,6 @@ $(document).ready(function() {
     });
   }
 
-  // Gets post data for the current post if we're editing, or if we're adding to an author's existing posts
   function getTaskData(id, type) {
     var queryUrl;
     switch (type) {
@@ -64,27 +63,20 @@ $(document).ready(function() {
     $.get(queryUrl, function(data) {
       if (data) {
         console.log(data.MemberId || data.id);
-        // If this post exists, prefill our cms forms with its data
         newTaskName.val(data.title);
         memberId = data.MemberId || data.id;
-        // If we have a post with this id, set a flag for us to know to update the post
-        // when we hit submit
         updating = true;
       }
     });
   }
 
-  // A function to get Authors and then render our list of Authors
   function getMembers() {
     $.get("/api/User", renderMemberList);
   }
-  // Function to either render a list of authors, or if there are none, direct the user to the page
-  // to create an author first
   function renderMemberList(data) {
     if (!data.length) {
       window.location.href = "/team";
     }
-    // $(".hidden").removeClass("hidden");
     var rowsToAdd = [];
     for (var i = 0; i < data.length; i++) {
       rowsToAdd.push(createMemberRow(data[i]));
@@ -96,7 +88,6 @@ $(document).ready(function() {
     memberSelect.val(memberId);
   }
 
-  // Creates the author options in the dropdown
   function createMemberRow(member) {
     var listOption = $("<option>");
     listOption.attr("value", member.id);
@@ -104,7 +95,6 @@ $(document).ready(function() {
     return listOption;
   }
 
-  // Update a given post, bring user to the blog page when done
   function updateTask(task) {
     $.ajax({
       method: "PUT",
