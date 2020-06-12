11d816c37e874bbed9e44715496cd699b7611a31
Karolyn Seredick <kseredick95@gmail.com>
2020-06-12 18:36:09 -0400

    added modal pop-up javascript

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,25 +1,47 @@
-$("#taskModal").css("display", "none");
+$("#addTaskBtn").on("click", function () {
+    
+    $("#taskModal").css("display", "none");
 
-$(document).ready(function() {
-    $("#taskModal").modal("hide");
+    $(document).ready(function () {
+        $("#taskModal").modal("hide");
 
-    // Stops modal from closing when clicked outside
-    $("#taskModal").modal({
-        backdrop: 'static',
-        keyboard: false
-    });
+        // Stops modal from closing when clicked outside
+        $("#taskModal").modal({
+            backdrop: 'static',
+            keyboard: false
+        });
 
+    })
 })
 
-$("#timeModal").css("display", "none");
+$("#addTimeBtn").on("click", function () {
 
-$(document).ready(function() {
-    $("#timeModal").modal("hide");
+    $("#timeModal").css("display", "none");
 
-    // Stops modal from closing when clicked outside
-    $("#timeModal").modal({
-        backdrop: 'static',
-        keyboard: false
-    });
+    $(document).ready(function () {
+        $("#timeModal").modal("hide");
 
+        // Stops modal from closing when clicked outside
+        $("#timeModal").modal({
+            backdrop: 'static',
+            keyboard: false
+        });
+
+    })
+})
+
+$("#addMemberBtn").on("click", function () {
+
+    $("#memberModal").css("display", "none");
+
+    $(document).ready(function () {
+        $("#memberModal").modal("hide");
+
+        // Stops modal from closing when clicked outside
+        $("#memberModal").modal({
+            backdrop: 'static',
+            keyboard: false
+        });
+
+    })
 })
