2e9517ea98bbadd87fc3239bf2805b934a19ee5d
Karolyn Seredick <kseredick95@gmail.com>
2020-06-13 11:03:50 -0400

    Moved files out of app folder, fixed html-routes

--- server.js
+++ server.js
@@ -3,10 +3,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-const db = require("./app/models")
-
-//Create files for each table in database -- UNCOMMENT AFTER DATABASE IS SET
-// const db = require("./app/models");
+const db = require("./models")
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,13 +11,13 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //Routing for API
-require("./app/routes/api-project")(app);
-require("./app/routes/api-task")(app);
-require("./app/routes/api-user")(app);
-require("./app/routes/api-time")(app);
+require("./routes/api-project")(app);
+require("./routes/api-task")(app);
+require("./routes/api-user")(app);
+require("./routes/api-time")(app);
 
 //Routing for HTML
-require("./app/routes/html-routes")(app);
+require("./routes/html-routes")(app);
 
 
 //Model has been setup
