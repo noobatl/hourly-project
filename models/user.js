92124d5468eba4e8883d514aa3fd5d0997929eb6
Nicole OBomsawin <nicoleobomsawin@gmail.com>
2020-06-15 17:57:50 -0400

    team members from jj

--- models/user.js
+++ models/user.js
@@ -1,42 +1,41 @@
-module.exports = function(sequelize, DataTypes){
-
-    const User = sequelize.define("User",{
-        userId:{
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
-        }, 
-        firstname: {
-            type:DataTypes.STRING,
-            allowNull: false
+// Creating our User model
+module.exports = function (sequelize, DataTypes) {
+    var User = sequelize.define("User", {
+      userId: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      firstname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
         },
-        lastname: {
-             type:DataTypes.STRING,
-             allowNull: false
-        }, 
-        role: {
-            type:DataTypes.STRING,
-             allowNull: false,
-             unique: false
-        },
-        email: {
-            type:DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                isEmail: true
-              }
-        }
+      },
     });
     User.associate = function (models) {
-        User.hasMany(models.Task, {
-        });
-
-        User.belongsToMany(models.Project, {
-            through: 'UserProject',
-        });
-    }
-
+      // Associating User with Projects
+      User.hasMany(models.Project, {});
+      // Associating User with Tasks
+      User.hasMany(models.Task, {});
+      User.belongsToMany(models.Project, {
+        through: "userProject",
+      });
+    };
     return User;
-}
-
+  };
