a3fdbc19a8f1a26f6feba24af40072d3d4e444f1
noobatl <info@noobusa.com>
2020-06-15 21:31:12 -0400

    edited task.js for multiple tasks

--- models/task.js
+++ models/task.js
@@ -1,36 +1,31 @@
 module.exports = function (sequelize, DataTypes) {
-    var Task = sequelize.define("Task", {
+  var Task = sequelize.define("Task", {
+    taskId: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    taskName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    taskDescription: {
+      type: DataTypes.TEXT,
+    },
+  });
 
-        taskId:{
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
-        }, 
-        taskName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-        },
-        taskDescription: {
-            type: DataTypes.TEXT
-        }
+  Task.associate = function (models) {
+    Task.belongsTo(models.Project, {
+      foreignKey: "projectId",
     });
+    Task.belongsTo(models.User, {
+      foreignKey: "userId",
+    });
+    Task.hasMany(models.TimeEntry, {
+      onDelete: "cascade",
+    });
+  };
 
-    Task.associate = function (models) {
-
-        Task.belongsTo(models.Project, {
-            foreignKey: 'projectId'
-        });
-        Task.belongsTo(models.User, {
-            foreignKey: 'userId'
-        });
-
-        Task.hasMany(models.TimeEntry, {
-            onDelete: "cascade"
-        });
-    };
-
-    return Task;
+  return Task;
 };
-
-
